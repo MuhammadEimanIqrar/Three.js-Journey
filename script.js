d2dc35668f54cc01fb37e2c61a63733201455aea
Muhammad Eiman Iqrar <meiman2485@gmail.com>
2024-03-26 00:03:26 +0500

    Group Practice

--- script.js
+++ script.js
@@ -11,59 +11,42 @@ const canvas = document.querySelector('canvas#webgl');
 // Scene:
 const scene = new THREE.Scene();
 
+// Groups:
+
+const group = new THREE.Group();
+
+scene.add(group);
+
 // Object:
 
 // We need to create a Mesh which requires 2 things:
 // 1. Geometry
 // 2. Material
 
-// Geometry:
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
 
-// Material:
-// Set {wireframe: true} to get objects wireframe
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+group.add(cube1);
 
-const mesh = new THREE.Mesh(geometry, material);
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
 
-// Object Rotation is Possible from four things:
-// 1. Position
-// 2. Scale
-// 3. Rotation
-// 4. Quaternion
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
 
-// 1. Position:
-
-// mesh.position.x = 1;
-// mesh.position.y = -0.5;
-// mesh.position.z = 1;
-
-// Shorthand:      x,  y,  z
-mesh.position.set(1, -0.5, 1);
-
-// 2. Scale:
-
-// mesh.scale.x = 1;
-// mesh.scale.y = -0.5;
-// mesh.scale.z = 1;
-
-// Shorthand:      x,  y,  z
-mesh.scale.set(1, -0.5, 1);
-
-// 3. Rotation:
-
-mesh.rotation.reorder('YXZ');
-// mesh.rotation.x = Math.PI;
-// mesh.rotation.y = Math.PI * 0.5;
-// mesh.rotation.z = Math.PI / 2;
-
-// Shorthand:      x,  y,  z
-mesh.rotation.set(Math.PI, Math.PI * 0.5, Math.PI / 2);
-
-// Method to check distance between center-point and object
-console.log(mesh.position.length());
-
-scene.add(mesh);
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
 
 // Sizes
 const sizes = {
@@ -77,13 +60,6 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 
 // We have to change the position of camera on z axis to see things, by default camera is positioned in the center of the scene and objects are not visible 
 camera.position.z = 3;
-
-// Method to check the distance between camera and cube
-console.log(camera.position.distanceTo(mesh.position));
-
-// Method for positioning camera on specific object
-
-// camera.lookAt(mesh.position);
 
 scene.add(camera);
 
